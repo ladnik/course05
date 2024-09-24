529cebb1c95fc9bade28cfa5f21623f5fa27c310
Tobias Pflüger <tobias.pflueger@tum.de>
2024-09-24 18:13:10 +0200

    Added simple height function mesh generator. (Output mesh is going in clockwise direction)

---
+++ project/terrain_manager.gd
@@ -0,0 +1,67 @@
+extends Node2D
+
+const TILE_SIZE = 16  # Specify the tile side length here
+
+var width = 400
+var height = 100
+
+var grid : Array = []
+var gridNoise = FastNoiseLite.new()
+
+
+func _ready():
+	generateGrid()
+	renderGrid()
+
+func generateGrid():
+	var returnBoundaries : Array[PackedVector2Array] = [] # all boundaries
+	var boundary : PackedVector2Array = [] # current boundary
+
+	# Generate grid array
+	for y in range(height):
+		var grid_row = []
+		for x in range(width):
+			grid_row.append(0)
+		grid.append(grid_row)
+
+	# Initialize noise generator
+	gridNoise.seed = 0815;
+	gridNoise.noise_type = FastNoiseLite.TYPE_PERLIN
+	gridNoise.fractal_octaves = 4
+	gridNoise.frequency = 0.0075
+
+	# Fill grid with noise data and generate output boundaries
+	for x in range(width):
+		var maxHeight = round(gridNoise.get_noise_1d(x) * height * 2 + height / 2.5)
+
+		boundary.append(Vector2(x, maxHeight))
+
+		print(maxHeight)
+		for y in range(height):
+			if maxHeight <= y:
+				grid[y][x] = 1
+
+	# Genereate the two bottom points
+	boundary.append(Vector2(width - 1, height - 1))
+	boundary.append(Vector2(0, 		   height - 1))
+
+	returnBoundaries.append(boundary)
+
+	return returnBoundaries
+
+
+func renderGrid(): 
+	for y in range(height):
+		for x in range(width):
+			var tile = ColorRect.new()
+			tile.size = Vector2(TILE_SIZE, TILE_SIZE)
+			tile.position = Vector2(x * TILE_SIZE, y * TILE_SIZE)
+			tile.color = Color.GRAY if grid[y][x] == 1 else Color.SKY_BLUE
+			add_child(tile)
+
+func _draw():
+	# Draw grid lines
+	for y in range(len(grid) + 1):
+		draw_line(Vector2(0, y * TILE_SIZE), Vector2(len(grid[0]) * TILE_SIZE, y * TILE_SIZE), Color.GRAY)
+	for x in range(len(grid[0]) + 1):
+		draw_line(Vector2(x * TILE_SIZE, 0), Vector2(x * TILE_SIZE, len(grid) * TILE_SIZE), Color.GRAY)
