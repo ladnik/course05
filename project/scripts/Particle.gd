740f7df1e2f932542ba6133704856e92f21f6b91
Jan Claar <mail@jan-claar.de>
2024-09-26 17:14:06 +0200

    constant changes

--- project/scripts/Particle.gd
+++ project/scripts/Particle.gd
@@ -6,11 +6,6 @@ var velocity = Vector2()
 var force = Vector2()
 var last_force = Vector2()
 
-
-var density = 0
-var pressure = 0
-var viscosity = 0
-
 func _init(pos = Vector2(),vel = Vector2()):
 	#set initial position and velocity
 	position = pos
