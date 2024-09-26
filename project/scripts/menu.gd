9e05412c74c9283106349f84cda65578a4e2dd6e
Christian Krinitsin <code@krinitsin.xyz>
2024-09-26 11:53:42 +0200

    pause menu redesign

--- project/scripts/menu.gd
+++ project/scripts/menu.gd
@@ -4,11 +4,9 @@ func start_gui_event(event: InputEvent) -> void:
 	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
 		get_tree().change_scene_to_file("res://scenes/test_level.tscn")
 
-
 func level_gui(event: InputEvent) -> void:
 	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
 		get_tree().change_scene_to_file("res://scenes/level_menu.tscn")
-
 
 func exit_gui_event(event: InputEvent) -> void:
 	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
--- project/scripts/menu_item.gd
+++ project/scripts/menu_item.gd
@@ -1,9 +1,9 @@
 extends Label
 
 func _on_mouse_entered() -> void:
-	var tween : Tween = get_tree().create_tween()
+	var tween : Tween = create_tween()
 	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.1).set_trans(Tween.TRANS_BOUNCE)
 
 func _on_mouse_exited() -> void:
-	var tween : Tween = get_tree().create_tween()
+	var tween : Tween = create_tween()
 	tween.tween_property(self, "scale", Vector2(1, 1), 0.1).set_trans(Tween.TRANS_BOUNCE)
