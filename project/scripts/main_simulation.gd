394b483312093d0fd99fdfeb7e9cb1249f017780
IngoSternberg <ingo.sternberg@fau.de>
2024-09-28 09:30:08 +0200

    add grid search and refactor

--- project/scripts/main_simulation.gd
+++ project/scripts/main_simulation.gd
@@ -9,53 +9,46 @@ var particle_mat = CanvasItemMaterial.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#View.size = Vector2(Constants.RENDER_WIDTH, Constants.RENDER_HEIGHT)
-	#OS.window_size = $View.size
 	SIM.mesh_generator = mesh_generator
-	'''
-	for p in SIM.fast_particle_array:
-			var waterdraw = Sprite2D.new()
-			waterdraw.add_to_group("water")
-			waterdraw.texture = preload("res://assets/ball.png")
-			waterdraw.scale *= Constants.PARTICLE_SIZE
-			waterdraw.material = particle_mat
-		
-			waterdraw.position = p * Constants.SCALE
-			add_child(waterdraw)
-	'''
-
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	SIM.update(delta)
-	#print(SIM.particles[1].position)
-	'''
-	var children = []
-	for child in get_children():
-			if child.is_in_group("water"):
-				children.append(child)
-				
-	print(SIM.fast_particle_array.size())
-	for p in range(SIM.fast_particle_array.size()):
-		var draw_point = children[p]
-		draw_point.position = (SIM.fast_particle_array[p] * Constants.SCALE)
-	'''
 
 	queue_redraw()
-		
-func _draw() -> void:
-	# Draw a rectangle outline
-	#draw_rect(Rect2(Vector2(0, 0), Vector2(Constants.RENDER_WIDTH, Constants.RENDER_HEIGHT)), Color(1, 1, 1), false)
-	#draw_line(Vector2(0,0), Vector2(Constants.RENDER_WIDTH, Constants.RENDER_HEIGHT), Color(1, 1, 0))
 
-	for p in range(SIM.fast_particle_array.size()):
-		draw_circle(SIM.fast_particle_array[p] * Constants.SCALE, 5 * Constants.SCALE, Color(0, 1, 0), true)
-		draw_circle(SIM.fast_particle_array[p] * Constants.SCALE, Constants.INTERACTION_RADIUS * Constants.SCALE, Color(0, 1, 0), false)
-		# draw diagonal line from particle to interaction boundary
-		draw_line(SIM.fast_particle_array[p] * Constants.SCALE, (SIM.fast_particle_array[p] + Vector2(1, 1).normalized() * Constants.INTERACTION_RADIUS) * Constants.SCALE, Color(1, 0, 0), 1, false)
-		if Constants.DISPLAY_VELOCITY:
-			draw_line(SIM.fast_particle_array[p] * Constants.SCALE, (SIM.fast_particle_array[p] + SIM.velocities[p]) * Constants.SCALE, Color(0, 1, 0), 1, false)
-		if Constants.DISPLAY_FORCE:
-			#print(SIM.particles[p].last_force)
-			draw_line(SIM.fast_particle_array[p] * Constants.SCALE, (SIM.fast_particle_array[p] + SIM.force_array[p]) * Constants.SCALE, Color(0, 0, 1), 1, false)
+
+func _draw() -> void:
+
+	# draw the particles
+	const pCol = Color(0, 0, 1)
+	for p in range(SIM.current_positions.size()):
+		draw_circle(SIM.current_positions[p] , 5 , pCol, true)
+
+
+	# draw the interaction radius etc for debugging
+	if Constants.DEBUG:
+
+		# draw the grid
+		if Constants.GRIDSEARCH:
+			const gCol = Color(0, 0, 1)
+			for x in range(0, Constants.WIDTH, Constants.GRID_SIZE):
+				draw_line(Vector2(x, 0) , Vector2(x, Constants.HEIGHT) , gCol, 1, false)
+			for y in range(0, Constants.HEIGHT, Constants.GRID_SIZE):
+				draw_line(Vector2(0, y) , Vector2(Constants.WIDTH, y) , gCol, 1, false)
+
+
+		const dCol = Color(1, 0, 0)
+		for p in range(SIM.current_positions.size()):
+			var pos = SIM.current_positions[p]
+			draw_circle(pos , Constants.INTERACTION_RADIUS , pCol, false)
+			
+			# draw diagonal line from particle to interaction boundary
+			draw_line(pos , (pos + Vector2(1, 1).normalized() * Constants.INTERACTION_RADIUS) , dCol, 1, false)
+
+			if Constants.DISPLAY_VELOCITY:
+				draw_line(pos , (pos + SIM.velocities[p]) , dCol, 1, false)
+			
+			if Constants.DISPLAY_FORCE:
+				draw_line(pos , (pos + SIM.forces[p]) , dCol, 1, false)
--- project/scripts/particle_simulation.gd
+++ project/scripts/particle_simulation.gd
@@ -6,58 +6,117 @@ var Constants = load('res://scripts/simulation_constants.gd')
 var WATER_SOURCE = load('res://scripts/water_source.gd')
 var water_source: Node2D
 
-var fast_particle_array = PackedVector2Array()
-var previous_positions = PackedVector2Array()
-var velocities = PackedVector2Array() 
-var force_array = PackedVector2Array()
+var current_positions: PackedVector2Array = PackedVector2Array()
+var previous_positions: PackedVector2Array = PackedVector2Array()
+var velocities: PackedVector2Array = PackedVector2Array() 
+var forces: PackedVector2Array = PackedVector2Array()
 
 var gravity_vector: Vector2 = Vector2(0, Constants.GRAVITY)
 var mesh_generator: MeshInstance2D
 
+var grid: Dictionary = {}
 
 
 # Called when the node enters the scene tree for the first time.
-
 func _init():
-	self.water_source = WATER_SOURCE.new(Vector2(100, 20), Vector2(0, 10), 10, 10, 0.2, 2, 0.0, 4.0)
+	if Constants.NUMBER_PARTICLES < 0:
+		self.water_source = WATER_SOURCE.new(Vector2(100, 20), Vector2(0, 10), 10, 10, 0.2, 2, 0.0, 4.0)
+	else:
+		random_spawn()
 
 
-func random_spawn():
+func get_neighbors(i:int)-> Array:
+	if Constants.GRIDSEARCH:
+		return grid_search(i)
+	else:
+		return direct_sum(i)
+
+func direct_sum(i:int) -> Array:
+	return range(i + 1, current_positions.size())
+
+# Convert world position to grid position
+func world_to_grid(pos: Vector2) -> Vector2:
+	return Vector2(floor(pos.x / Constants.GRID_SIZE), floor(pos.y / Constants.GRID_SIZE))
+
+# Build grid based on particle positions
+func build_grid() -> void:
+	for i in range(current_positions.size()):
+		var grid_pos = world_to_grid(current_positions[i])
+
+		if grid.has(grid_pos):
+			grid[grid_pos].append(i)
+		else:
+			grid[grid_pos] = [i]
+
+
+func grid_search(i:int) -> Array:
+	if grid.is_empty():
+		build_grid()
+
+	var neighbors = []
+
+	var pos = world_to_grid(current_positions[i])
+	var ownCell = grid[pos]
+	var posInOwnCell = ownCell.find(i)
+	# get the neighbors that come after the index of the current particle from the ownCell
+	for j in range(posInOwnCell + 1, ownCell.size()):
+		neighbors.append(ownCell[j])
+	
+	# get the neighbors from the cells to the right and bottom of the current cell
+	for  j in range(0, 2):
+		for k in range(0, 2):
+			if j == 0 and k == 0:
+				continue
+
+			var neighborPos = pos + Vector2(j, k)
+			if grid.has(neighborPos):
+				neighbors += grid[neighborPos]
+
+	return neighbors
+
+
+func random_spawn() -> void:
 	for i in range(Constants.NUMBER_PARTICLES):
-		var position = Vector2(randf() * 50+100, randf() * 0)
-		fast_particle_array.push_back(position)
+		var position = Vector2(randf() * 200, randf() * 200)
+		current_positions.push_back(position)
 		previous_positions.push_back(position)
 		velocities.push_back(Vector2(0,0))
-		force_array.push_back(Vector2(0,0))
+		forces.push_back(Vector2(0,0))
 
 
 
-func update(delta):
-	self.water_source.spawn(delta, fast_particle_array, previous_positions, velocities, force_array)
+func update(delta) -> void:
+	if Constants.NUMBER_PARTICLES < 0:
+		self.water_source.spawn(delta, current_positions, previous_positions, velocities, forces)
 
+	# reset everything
 	reset_forces()
+	grid.clear()
+	
+	# calculate the next step
 	calculate_interaction_forces()
 	integration_step(delta)
-	double_density_relaxation(delta)
+	
+	#double_density_relaxation(delta)
 	check_oneway_coupling()
 	calculate_next_velocity(delta)
 	
-	clipToBorder()
+	bounceFromBorder()
 
-func integration_step(delta):
-	for i in range(fast_particle_array.size()):
-		var force: Vector2 = gravity_vector + force_array[i]
+func integration_step(delta) -> void:
+	for i in range(current_positions.size()):
+		var force: Vector2 = gravity_vector + forces[i]
 		#var force: Vector2 = gravity_vector 
-		previous_positions[i] = fast_particle_array[i]
+		previous_positions[i] = current_positions[i]
 		velocities[i] += delta * force
-		fast_particle_array[i] += delta * velocities[i]
+		current_positions[i] += delta * velocities[i]
 	
 
 	
-func calculate_next_velocity(delta):
-	for i in range(fast_particle_array.size()):
+func calculate_next_velocity(delta) -> void:
+	for i in range(current_positions.size()):
 		#Calculate the new velocity from the previous and current position
-		var velocity : Vector2 = (fast_particle_array[i] - previous_positions[i]) / delta
+		var velocity : Vector2 = (current_positions[i] - previous_positions[i]) / delta
 		velocities[i] = velocity
 
 func interaction_force(position1, position2) -> Vector2:
@@ -76,51 +135,51 @@ func interaction_force(position1, position2) -> Vector2:
 	
 func calculate_interaction_forces() -> void:
 	# sum over all particles without double counting
-	for i in range(fast_particle_array.size()):
-		for j in range(i+1, fast_particle_array.size()):
-			var force = interaction_force(fast_particle_array[i], fast_particle_array[j])
-			force_array[i] -= force
-			force_array[j] += force
+	for i in range(current_positions.size()):
+		for j in get_neighbors(i):
+			var force = interaction_force(current_positions[i], current_positions[j])
+			forces[i] -= force
+			forces[j] += force
 
 func reset_forces():
-	for i in range(fast_particle_array.size()):
-		force_array[i] = Vector2(0,0)
+	for i in range(current_positions.size()):
+		forces[i] = Vector2(0,0)
 
 func collision_checker(i:int)-> Array:
-	#print(fast_particle_array[i].x /2)
-	#if fast_particle_array[i].y >fast_particle_array[i].x /2:
+	#print(current_positions[i].x /2)
+	#if current_positions[i].y >current_positions[i].x /2:
 		#return true
 	#else:
 		#return false
-	var array_collision = mesh_generator.continuous_collision(previous_positions[i], fast_particle_array[i])
+	var array_collision = mesh_generator.continuous_collision(previous_positions[i], current_positions[i])
 	return array_collision
-		
-func check_oneway_coupling():
-	for i in range(fast_particle_array.size()):
+
+func check_oneway_coupling() -> void:
+	for i in range(current_positions.size()):
 		var collision_object = collision_checker(i)
 		if collision_object[0] == true:
 			#print("True")
-			#fast_particle_array[i]+=Vector2(2,-4).normalized()/Constants.SCALE/15
-			# fast_particle_array.remove_at(i)
+			#current_positions[i]+=Vector2(2,-4).normalized()/Constants.SCALE/15
+			# current_positions.remove_at(i)
 			# previous_positions.remove_at(i)
 			# velocities.remove_at(i)
-			# force_array.remove_at(i)
-			fast_particle_array[i]+=collision_object[2].normalized()/Constants.SCALE
+			# forces.remove_at(i)
+			current_positions[i]+=collision_object[2].normalized()
 
 
-func double_density_relaxation(delta):
-	for i in range(fast_particle_array.size()):
+func double_density_relaxation(delta) -> void:
+	for i in range(current_positions.size()):
 		var density = 0
 		var density_near = 0
-		var particleA= fast_particle_array[i]
+		var particleA= current_positions[i]
 		var h = 30 #cut-off radius
 		var k = 0.1 
 		var k_near= 0.2
 		var density_zero= 10
-		for j in range(fast_particle_array.size()):
+		for j in range(current_positions.size()):
 			if i==j:
 				continue
-			var particleB=fast_particle_array[j]
+			var particleB=current_positions[j]
 			var rij = particleB-particleA
 			var q=rij.length()/h
 			if q < 1:
@@ -130,36 +189,34 @@ func double_density_relaxation(delta):
 		var pressure= k*(density-density_zero)
 		var pressure_near= k_near*density_near
 		var pos_displacement_A = Vector2(0,0)
-		for j in range(fast_particle_array.size()):
+		for j in range(current_positions.size()):
 			if i==j:
 				continue
-			var particleB=fast_particle_array[j]
+			var particleB=current_positions[j]
 			var rij = particleB-particleA
 			var q=rij.length()/h
 			if q < 1:
 				rij=rij.normalized()
 				var displacement_term:Vector2 =delta**2 * (pressure*(1-q)+pressure_near*(1-q)**2)*rij
-				fast_particle_array[j] += displacement_term/2
+				current_positions[j] += displacement_term/2
 				pos_displacement_A -= displacement_term/2
-		fast_particle_array[i] += pos_displacement_A
-				
-		
-			
-			
-func clipToBorder():
-	for i in range(fast_particle_array.size()):
-		if fast_particle_array[i].x < 0:
-			fast_particle_array[i].x = 0
+		current_positions[i] += pos_displacement_A
+
+
+func bounceFromBorder() -> void:
+	for i in range(current_positions.size()):
+		if current_positions[i].x < 0:
+			current_positions[i].x = 0
 			velocities[i].x *= -1
-		if fast_particle_array[i].x > Constants.WIDTH:
-			fast_particle_array[i].x = Constants.WIDTH
+		if current_positions[i].x > Constants.WIDTH:
+			current_positions[i].x = Constants.WIDTH
 			velocities[i].x *= -1
-		if fast_particle_array[i].y < 0:
-			fast_particle_array[i].y = 0
+		if current_positions[i].y < 0:
+			current_positions[i].y = 0
 			velocities[i].y *= -1
-		if fast_particle_array[i].y > Constants.HEIGHT:
-			fast_particle_array[i].y = Constants.HEIGHT
-			velocities[i].y = 0
+		if current_positions[i].y > Constants.HEIGHT:
+			current_positions[i].y = Constants.HEIGHT
+			velocities[i].y *= -1
 
 func get_particle_positions():
-	return fast_particle_array
+	return current_positions
