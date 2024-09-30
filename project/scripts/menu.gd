8b2775cccb011269481511a2ae11883209d2f0de
Rohitkalla <rohitjunnn3@gmail.com>
2024-09-30 13:49:32 +0200

    credit button added in main menu

--- project/scripts/menu.gd
+++ project/scripts/menu.gd
@@ -11,3 +11,8 @@ func level_gui(event: InputEvent) -> void:
 func exit_gui_event(event: InputEvent) -> void:
 	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
 		get_tree().quit()
+
+
+func _on_credits_gui_input(event: InputEvent) -> void:
+	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
+		get_tree().change_scene_to_file("res://scenes/credits.tscn")
