e16c2ca6edfc0c08c999e45f8a48a03209b42382
DPSchenk <daniel.schenk@online.de>
2024-10-02 17:51:10 +0200

    voronoi tesselation ground

--- project/scripts/visualisation/particle_visualization.gd
+++ project/scripts/visualisation/particle_visualization.gd
@@ -3,9 +3,34 @@ extends Sprite2D
 @onready var simulation = $".."
 @onready var terrain = $"../../TerrainManager/TerrainEditor"
 var stored_grid
+var rng = RandomNumberGenerator.new()
+var colors = [
+    Color(1.0, 0.82, 0.86, 1),  # Light Pink
+    Color(0.8, 1.0, 0.9, 1),    # Mint Green
+    Color(0.9, 0.9, 0.98, 1),   # Lavender
+    Color(1.0, 0.99, 0.82, 1),  # Light Yellow
+    Color(0.88, 0.94, 1.0, 1),  # Baby Blue
+    Color(1.0, 0.89, 0.77, 1),  # Peach
+    Color(0.85, 0.75, 0.85, 1), # Thistle
+    Color(0.94, 1.0, 0.94, 1),  # Honeydew
+    Color(0.96, 0.87, 0.7, 1),  # Light Orange
+    Color(0.87, 0.98, 0.91, 1), # Mint Cream
+    Color(0.95, 0.87, 0.95, 1), # Light Purple
+    Color(0.93, 0.9, 0.79, 1)   # Light Khaki
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var voronoi_img = Image.create(10, 1, false, Image.FORMAT_RGBA8)
+	for i in range(10):
+		voronoi_img.set_pixel(i, 0, colors[i])
+	var itex = ImageTexture.create_from_image(voronoi_img)
+	material.set_shader_parameter("voronoi_colors", itex)
+	var voronoi_points = PackedVector2Array()
+	voronoi_points.resize(128)
+	for i in range(len(voronoi_points)):
+		voronoi_points[i] = Vector2(rng.randf_range(0.0, 1920.0), rng.randf_range(0.0, 1080.0))
+	material.set_shader_parameter("voronoi_centers", voronoi_points)
 	pass # Replace with function body.
 
 
