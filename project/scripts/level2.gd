daebf918bb233a4805d8e37c80c1b07f4195e922
DPSchenk <daniel.schenk@online.de>
2024-10-02 10:27:28 +0200

    remove debug printing, adapt level 2

--- project/scripts/level2.gd
+++ project/scripts/level2.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 @onready var terrain_manager: Node2D = $TerrainManager
+var particle_simulation_resource = load("res://scenes/particle_simulation.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	terrain_manager.generate_terrain(13, FastNoiseLite.TYPE_PERLIN, 4, 0.0075)
-	self.add_child(ParticleSimulation.new(200, 50, 0, 0))
+	var particle_simulation = particle_simulation_resource.instantiate()
+	
+	add_child(particle_simulation)
+	particle_simulation.set_init_data(200, 50, 0, 0)
+
--- project/scripts/particle_visualization.gd
+++ project/scripts/particle_visualization.gd
@@ -4,9 +4,11 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("visualization exists")
+	pass
+	#print("visualization exists")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(simulation.SIM.get_particle_positions())
+	pass
+	#print(simulation.SIM.get_particle_positions())
