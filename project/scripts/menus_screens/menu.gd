e1cd0a4667284f3da08e6a3f409c082fa3da4c55
Yevhenii Karpushchenko <karpuskarp@gmail.com>
2024-10-03 13:39:54 +0200

    Added click to start label

--- project/scripts/menus_screens/menu.gd
+++ project/scripts/menus_screens/menu.gd
@@ -7,6 +7,7 @@ extends CanvasLayer
 @onready var intro_scene: VideoStreamPlayer = $IntroScene
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var showHint = false
 
 func _ready() -> void:
 	AudioManager.play_main_menu_music()
@@ -39,8 +40,26 @@ func _on_intro_scene_click(event: InputEvent) -> void:
 	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
 		#print("clicked")
 		if intro_scene != null and intro_scene.paused == true:
+			var tween : Tween = create_tween()
+			tween.parallel().tween_property($ClickToStart, "label_settings:font_color:a", 0, 0.2).set_trans(Tween.TRANS_QUAD)
+			tween.parallel().tween_property($ClickToStart, "label_settings:shadow_color:a", 0, 0.2).set_trans(Tween.TRANS_QUAD)
+			tween.parallel().tween_property($ClickToStart, "label_settings:outline_color:a", 0, 0.2).set_trans(Tween.TRANS_QUAD)
+			
+			await tween.finished
+			tween.kill()
+			
 			animation_player.play("buttons_in")
 			intro_scene.paused = false
+
+func _process(delta: float) -> void:
+	if !showHint and intro_scene != null and intro_scene.paused == true:
+		showHint = true
+		var tween : Tween = create_tween()
+		tween.parallel().tween_property($ClickToStart, "label_settings:font_color:a", 1, 0.2).set_trans(Tween.TRANS_QUAD)
+		tween.parallel().tween_property($ClickToStart, "label_settings:shadow_color:a", 1, 0.2).set_trans(Tween.TRANS_QUAD)
+		tween.parallel().tween_property($ClickToStart, "label_settings:outline_color:a", 1, 0.2).set_trans(Tween.TRANS_QUAD)
+		await tween.finished
+		tween.kill()
 
 func _on_intro_scene_finished():
 	background_scene.play()
